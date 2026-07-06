2cba5295ec64653e128f7fb3dc0f88d55b4f192a
lbs <sensems@163.com>
2026-07-06 14:56:38 +0800

    feat: wire MediaModule into WechatModule and AppModule

--- src/wechat/wechat.module.ts
+++ src/wechat/wechat.module.ts
@@ -5,11 +5,13 @@ import { WechatService } from './wechat.service';
 import { WechatAccessTokenService } from './wechat-access-token.service';
 import { WechatMessageProcessor } from '../queue/processors/wechat-message.processor';
 import { StorageModule } from '../storage/storage.module';
+import { MediaModule } from '../media/media.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({ name: 'wechat-message' }),
     StorageModule,
+    MediaModule,
   ],
   controllers: [WechatController],
   providers: [WechatService, WechatAccessTokenService, WechatMessageProcessor],
